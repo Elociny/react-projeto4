334642c20b949689ccbb94beca342714ffe66cc6
Nicole Lins Coelho <nicolelinscoelho@gmail.com>
2023-12-04 00:21:45 -0300

    Projeto 4 em react

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './index.css'
+
+import Calculator from './components/Calculator'
+
+function App() {
+
+  return (
+    <div className="app">
+      <h1 className="titulo"> Calculadora</h1>
+      <Calculator />
+    </div>
+  )
+}
+
+export default App
